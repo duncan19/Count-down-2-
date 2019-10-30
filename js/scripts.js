644e3488203fc7ee65f4969c976aada4eb4c5981
Student <example@example.com>
2019-10-30 10:54:03 -0700

    fixed some bugs

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,38 @@ $(document).ready(function(){
       console.log("test")
       var usersnumcahnge = usersNum
       for(i=1; i <= usersNum; i++){
-        userNumArr.push(usersnumcahnge);
-        usersnumcahnge -= 1;
+        userNumArr.push(usersnumcahnge)
 
+        usersnumcahnge -= 1;
         }
-        // var usersnumcahnge = usersNum
-        // userNumArr.push(usersnumcahnge)
+        for (i = 0; i < userNumArr.length; i ++){
+         if(userNumArr[i].toString().includes(3))
+         {
+           userNumArr[i] = 'Happy3';
+         }
+         else if (userNumArr[i].toString().includes(2))
+         {
+           userNumArr[i] = 'Happy2';
+         }
+         else if (userNumArr[i].toString().includes(1))
+         {
+           userNumArr[i] = 'Happy1';
+         }
+
+            // var usersnumcahnge = usersNum
+            // userNumArr.push(usersnumcahnge)
         // var usersnumcahnge = usersnumcahnge - 1;
       };
-      console.log(numberSplit())
+
       console.log(userNumArr)
-    });
+      // userNumArr.forEach(function(){
+      //   if(userNumArr === 3)
+      //
+      //
+      // })
+    };
 
-
+    console.log(numberSplit())
+});
 
 });
